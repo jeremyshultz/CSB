649fd6e41e639cda909568b769509382a90880da
jeremyshultz <shultz.jeremy@gmail.com>
2019-03-14 10:51:40 -0500

    Update LinkedInTalentBlog.js

--- LinkedInTalentBlog.js
+++ LinkedInTalentBlog.js
@@ -1,16 +1,16 @@
 https://www.rssdog.com/index.php?url=http%3A%2F%2Ffeeds.feedblitz.com%2Ftalentblog%26x%3D1&amp;mode=javascript&amp;showonly=&amp;maxitems=5&amp;showdescs=1&amp;desctrim=0&amp;descmax=0&amp;tabwidth=100%25&amp;linktarget=_blank&amp;textsize=medium&amp;bordercol=%23d4d0c8&amp;headbgcol=%23999999&amp;headtxtcol=%23ffffff&amp;titlebgcol=%23f1eded&amp;titletxtcol=%23000000&amp;itembgcol=%23ffffff&amp;itemtxtcol=%23000000&amp;ctl=0
 
 (function(){
-	var elements = document.getElementsByTagName( 'img' );
+	var elements = document.getElementsByTagName( 'a.rssdog' );
 	element = elements[ 0 ];
 
 	// Specific old IE
 	if ( document.all ) {
-		element.style.setAttribute( 'cssText', 'marginBottom: 20px !important' );
+		element.style.setAttribute( 'cssText', 'color: blue !important' );
 
 	// Modern browser
 	} else {
-		element.setAttribute( 'style', 'marginBottom: 20px !important' );
+		element.setAttribute( 'style', 'color: blue !important' );
 	}
 })();
 
